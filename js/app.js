7dbc67687c69d58f77e9bc9f2ab8ac9d9a7e8d0b
sarahlikeshiny <sarah.g.thomas@gmail.com>
2017-02-15 16:31:02 +0000

    timed mode working

--- js/app.js
+++ js/app.js
@@ -2,18 +2,6 @@ console.log('js is connected - woo');
 
 $(() => {
 
-
-//pseudocode
-// reset from last game, blank all
-// create an array of words and grab events using jquery
-// randomly select a word from the Array
-// symbolise letters by an underscore from the array. must deal with spaces between words.
-// user submits a letter using text input and button (could set up keyboard listener, stick to button for MVP)
-// check letter against the work
-// if letter is present display on the screen
-// if not add part of gallows and add letter to screen, and update incorrect turns counter
-// continue until all turns are used or word is guessed.
-
 //global constants
   const words = ['cat', 'dog', 'horse', 'penguin', 'monkey'];
   const $displayWord =$('.word');
@@ -23,25 +11,21 @@ $(() => {
   const $reset=$('#reset');
   const $winLoseMsg = $('.word');
   const $picture = $('img');
+  const $timedMode= $('#timed');
+  const $showtimer = $('.timer');
   let userLetter= '';
   let correctCharsSpace =[];
-  const $images = new Array();
-  // $images[0]= new Image();
-  // $images[0].src='./images/step-zero.png';
-  $images[1]= new Image();
-  $images[1].src='./images/step-one.png';
-  $images[2]= new Image();
-  $images[2].src='./images/step-two.png';
-  $images[3]= new Image();
-  $images[3].src='./images/step-three.png';
-  $images[4]= new Image();
-  $images[4].src='./images/step-four.png';
-  $images[5]= new Image();
-  $images[5].src='./images/step-five.png';
-  $images[6]= new Image();
-  $images[6].src='./images/step-six.png';
-  $images[7]= new Image();
-  $images[7].src='./images/step-seven.png';
+  let timerOn = false;
+  const images = [
+    'step-zero.png',
+    'step-one.png',
+    'step-two.png',
+    'step-three.png',
+    'step-four.png',
+    'step-five.png',
+    'step-six.png',
+    'step-seven.png'
+  ];
 
 
 
@@ -50,19 +34,35 @@ $(() => {
   $reset.on('click', function () {
     location.reload(true);
   });
+//listen for click to activate timed mode.
+  $timedMode.on('click', startStopTimer);
 
 //select word at random
   const currentWord = words[Math.floor(Math.random() * words.length)];
   console.log(currentWord);
 
 //create underscores for display
+
+
   const underScores = currentWord.replace(/[a-z]/g, ' _');
   $displayWord.text(underScores);
   // console.log(underScores.length);
   const underScoresNoWhite = underScores.replace(/\s/g, '');
   // console.log(underScoresNoWhite.length);
+  // (function typeWriter() {
+  //   const timeOut = setTimeout(function() {
+  //     underScores++;
+  //     var type = underScores.substring(0, underScores);
+  //     $displayWord.text(type);
+  //     typeWriter();
+  //
+  //     if (underScores === length) {
+  //       clearTimeout(timeOut);
+  //     }
+  //
+  //   }, 110);
 
-
+  // }());
 
 //click guess button, capture user input, check for duplicates
 
@@ -111,94 +111,70 @@ $(() => {
     if (indices.length === currentWord.length) {
       console.log('win');
       $winLoseMsg.text('You Win!');
+    } //the timer version need to go here, need to do this bit unless the timer is pressed then do the other bit.
+    if (timerOn === false) {
+      const image = `images/${images[incorrectChars.length]}`;
+      $picture.attr('src', image);
+      if(incorrectChars.length === 7) {
+        $winLoseMsg.text('Sorry You Lose');
+      }
     }
-    //display pictures.
-    if (incorrectChars.length === 1){
-      $picture.attr($images[1]);
-    } else if (incorrectChars.length === 2){
-      //picture 2
-      $picture.attr($images[2]);
-    } else if (incorrectChars.length === 3){
-      //picture 3
-      $picture.attr($images[3]);
-    } else if (incorrectChars.length === 4){
-      //picture 4
-      $picture.attr($images[4]);
-    } else if (incorrectChars.length === 5){
-      //picture 5
-      $picture.attr($images[5]);
-    } else if (incorrectChars.length === 6){
-      //picture 6
-      $picture.attr($images[6]);
-    } else if (incorrectChars.length === 7){
-      //picture 7
-      $picture.attr($images[7]);
-      $winLoseMsg.text('Sorry You Lose');
-    }
-
   });
 
+// ------------------TIMER----------------------------
 
-  // timed mode
-  // on button press, activate timer,every 5 secs lose a life, for every correct answer pause clock for 5 secs.
-
-  // ------------------TIMER----------------------------
-  const $timedMode= $('#timed');
-  const $timer = $('.timer');
-  // // const $timerScreen = $timer.find('.screen');
-  // // const $startStopBtn = $timer.find('#startStop');
-  // // const $resetBtn = $timer.find('#reset');
-  //
-  //
-  // // add event listeners to $startStopBtn & $resetBtn
-  //
   let timeRemaining = 30;
   let timerIsRunning = false;
   let timerId = null;
 
 
-  function startStopTimer() {
-    // stop the timer if it is running
-    if(timerIsRunning) {
-      clearInterval(timerId);
-      timerIsRunning = false;
-    } else {
-      // start the timer if it is NOT running
-      timerId = setInterval(() => {
-        timeRemaining--;
-        $timer.text(timeRemaining);
-//stop timer at 0 and change image to final and add lose message
-        if(timeRemaining === 0) {
-          clearInterval(timerId);
-          $picture.attr('src','../images/step-seven.png');
-          $winLoseMsg.text('Sorry You Lose');
-        }
-      }, 1000);
-      timerIsRunning = true;
-    }
+//   function startStopTimer() {
+//   // stop the timer if it is running
+//     if(timerIsRunning) {
+//       clearInterval(timerId);
+//       timerIsRunning = false;
+//     } else {
+//     // start the timer if it is NOT running
+//       timerId = setInterval(() => {
+//         timeRemaining--;
+//         $showtimer.text(timeRemaining);
+// //stop timer at 0 and change image to final and add lose message
+//         if(timeRemaining === 0) {
+//           clearInterval(timerId);
+//           $picture.attr('src','../images/step-seven.png');
+//           $winLoseMsg.text('Sorry You Lose');
+//         }
+//       }, 1000);
+//       timerIsRunning = true;
+//     }
+//   }
+
+  // // ------------SPEED MODE FUNCTION------------
+  function startStopTimer (){
+    console.log('startStopTimer');
+    let i = 1;
+    const timerId = setInterval(() => {
+      $picture.attr('src', `images/${images[i]}`);
+
+      if(i === 7) {
+        clearInterval(timerId);
+        console.log('Game over!');
+      }
+
+      i++;
+    }, 500);
+
   }
 
-  //------------SPEED MODE FUNCTION------------
-  // function changeImage (){
-  //   for (let i = 0; i<= $images.length; i++ ){
-  //     $picture.attr('src', $images[i]);
-  //     console.log($images[i]);
-  //   }
-  //   setInterval(changeImage,5000);
+  // if (timerOn === true) {
+  //   changeImage();
+  //   startStopTimer();
   // }
 
-
-//fade next image in every 5 seconds
-  // let $img = $images, i = 0, speed = 100;
-  // window.setInterval(function() {
-  //     $img.attr('src', $images[(++i % $images.length)]);
-  //   });
-  // }, 5000);
-  $timedMode.on('click', startStopTimer);
-  // $timedMode.on('click', changeImage);
+  // $timedMode.on('click', startStopTimer);
+// $timedMode.on('click', changeImage);
 
 });
-
 
 //once hangman is hanged dont allow any more letters to be input
 //once a letter is used dont allow this to be input against
